ea027b35389b2331f34028135fc32778f0898eb9
abrar <abrar@therapservices.net>
2019-06-18 23:02:26 +0600

    Changed the sources for all images and added main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+function instagram(){
+    window.open('https://www.instagram.com/abrarhayat/?hl=en', '_blank');
+}
+
+function flickr(){
+    window.open('https://www.flickr.com/photos/125138217@N08/', '_blank');
+}
+
+function linkedIn(){
+    window.open('https://www.linkedin.com/in/abrar-hayat-56a780131', '_blank');
+}
