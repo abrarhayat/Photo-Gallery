e0ae9f103e4649ac380d82d16d23feb6f325cb86
Abrar Hayat <abrarhayat@gmail.com>
2021-04-05 00:00:57 +0600

    Changed logo and social buttons

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 
 
-function instagram(){
+const instagram = () => {
     window.open('https://www.instagram.com/abrarhayat/?hl=en', '_blank');
 }
 
-function flickr(){
+const flickr = () => {
     window.open('https://www.flickr.com/photos/125138217@N08/', '_blank');
 }
 
-function linkedIn(){
+const linkedIn = () => {
     window.open('https://www.linkedin.com/in/abrar-hayat-56a780131', '_blank');
 }
+
+const github = () => {
+    window.open('https://github.com/abrarhayat', '_blank');
+}
+
+$('#footer-text').html(`© ${new Date().getFullYear()} Abrar Hayat`)
